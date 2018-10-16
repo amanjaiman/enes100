b2db7d31ddcf0600f10ff942ff2f33fff83fdfb9
Aman Jaiman <amanjaiman@outlook.com>
2018-10-16 16:19:31 -0400

    Add destination and location retrieval

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,26 @@
+#include "Enes100.h"
+
+int dest_x;
+int dest_y;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  while (!enes.retrieveDestination()) {
+    enes.println("Unable to retrieve location");
+  }
+  dest_x = enes.destination.x;
+  dest_y = enes.destination.y;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int my_x;
+  int my_y;
+  float my_theta;
+  // Update OSV location
+  if (enes.updateLocation()) {
+    my_x = enes.location.x;
+    my_y = enes.location.y;
+    my_theta = enes.location.theta;
+  } else {
+    enes.println("Error: Couldn't update location")
+  }
 }
