b3560a1bd48e14264453cb01c1c275d1cf08a821
Aman Jaiman <amanjaiman@outlook.com>
2018-10-16 19:51:05 -0400

    Add forward and backward movement

--- main/main.ino
+++ main/main.ino
@@ -5,11 +5,13 @@
 //Enes100 enes("Ironsight", DEBRIS, 3, 8, 9);
 
 Enes100Simulation enes;
+DFRTankSimulation tank;
 
 int dest_x;
 int dest_y;
 int my_x;
 int my_y;
+float my_theta;
 
 void setup() {
   while (!enes.retrieveDestination()) {
@@ -19,17 +21,28 @@ void setup() {
   dest_y = enes.destination.y;
 }
 
-void loop() {
-  float my_theta;
-  
-  // Update OSV location
+void updateOSVLocation() {
   while(!enes.updateLocation()) {
     enes.println("Error: Could not update location");
   }
   my_x = enes.location.x;
   my_y = enes.location.y;
   my_theta = enes.location.theta;
+}
 
-  
-  
+void moveForward() {
+  tank.setLeftMotorPWM(255);
+  tank.setRightMotorPWM(255);
+}
+
+void moveBackward() {
+  tank.setLeftMotorPWM(-255);
+  tank.setRightMotorPWM(-255);
+}
+
+void loop() {
+
+  //updateOSVLocation();
+  moveForward();
+
 }
